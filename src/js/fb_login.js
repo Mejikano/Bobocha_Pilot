47e02627c8383a7d3d6a8b15f7b288707daf0744
Mejikano <kano_mejikano@yahoo.com.mx>
2022-04-18 15:26:36 -0400

    update

--- src/js/fb_login.js
+++ src/js/fb_login.js
@@ -2,9 +2,9 @@
   console.log("login script");
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
-    console.log("hello world");
-    console.log(response.status);
-    console.log(response);
+    //console.log("hello world");
+    //console.log(response.status);
+    //console.log(response);
   });
 
   
