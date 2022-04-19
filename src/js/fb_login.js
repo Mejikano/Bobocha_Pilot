4a0a7dbcebb8a63be40ee6c3d60bb02035fbb048
Mejikano <kano_mejikano@yahoo.com.mx>
2022-04-18 21:02:19 -0400

    update

--- src/js/fb_login.js
+++ src/js/fb_login.js
@@ -1,3 +1,23 @@
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '689321112121618',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'Beta'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
 function checkLoginState() {
   console.log("login script");
   FB.getLoginStatus(function(response) {
